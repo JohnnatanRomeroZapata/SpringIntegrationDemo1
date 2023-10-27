d29f1d42ec6f0774488d047e1119a5c2ddb763d4
romj2504 <Johnnatan.Romero.Zapata@USherbrooke.ca>
2023-10-27 18:20:59 -0400

    Project init

---
+++ src/main/java/com/example/SpringIntegrationDemo1/SpringIntegrationDemo1Application.java
@@ -0,0 +1,13 @@
+package com.example.SpringIntegrationDemo1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringIntegrationDemo1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringIntegrationDemo1Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/SpringIntegrationDemo1/SpringIntegrationDemo1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.SpringIntegrationDemo1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringIntegrationDemo1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
